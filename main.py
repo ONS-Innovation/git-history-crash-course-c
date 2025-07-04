6c7806b019a529fcc9b0a8c5ed2ddd685a63f019
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-07-04 14:48:54 +0100

    Print AWS key for debugging

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    # Access the secret from the environment variable
+    aws_secret = "AWS Secret - my-secret-value"
+
+    print(f"Accessed secret: {aws_secret}")
+        
